8ed353500da92a89d1946272d959fc59c62078e0
FIC <FIC@info.local>
2020-08-31 22:54:14 -0300

    Alterações no componente section, e inserção de novas imagens.

--- frontend/src/app/components/template/main/main.component.ts
+++ frontend/src/app/components/template/main/main.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { SectionComponent } from '../section/section.component';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
+
+  
 })
 export class MainComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  
+  constructor() { 
+    // abrirModal{
+    //   document.getElementById("abrirModal").classList.add("mostrar");
+    // }
   }
-
+  ngOnInit(): void {  }
+  
 }
